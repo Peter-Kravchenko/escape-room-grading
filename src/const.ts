dff81b51dd4fe97770a85390d27394ec860cd5ba
Петр <kravchenko.pb.13@gmail.com>
2024-03-11 14:07:42 +0300

    Исправит url карты

--- src/const.ts
+++ src/const.ts
@@ -3,10 +3,10 @@ import { IconConfig } from './types/map';
 export const DESCRIPTION_LENGTH = 300;
 
 export const TILE_LAYER =
-  'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png';
+  'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png';
 
 export const COPYRIGHT =
-  '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>';
 
 export const officeIconConfig: IconConfig = {
   url: 'public/img/svg/pin-active.svg',
